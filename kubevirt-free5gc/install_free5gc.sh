6876de1e1e077c87e582863d5ac608f68d7ff8d9
Jerry <tw0927041027@gmail.com>
2020-01-05 20:17:18 +0800

    fix cmd error

--- kubevirt-free5gc/install_free5gc.sh
+++ kubevirt-free5gc/install_free5gc.sh
@@ -27,16 +27,16 @@ apt-get install -y \
 
 apt-get clean 
 
-wget https://dl.google.com/go/go1.11.4.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.11.4.linux-amd64.tar.gz
-export GOROOT=/usr/local/go
-export GOPATH=/root/go
-export PATH=$PATH:/usr/local/go/bin
+wget -q https://storage.googleapis.com/golang/getgo/installer_linux
+chmod +x installer_linux
+./installer_linux
+source ~/.bash_profile
+rm -f installer_linux
 go get -u -v "github.com/gorilla/mux"
 go get -u -v "golang.org/x/net/http2"
 go get -u -v "golang.org/x/sys/unix"
-git clone https://bitbucket.org/nctu_5g/free5gc.git
-git clone https://github.com/tw0927041027/OpenStack-Tacker-Exercise -b kubevirt
+
+git clone https://bitbucket.org/nctu_5g/free5gc-stage-1.git
 cd /free5gc
 autoreconf -iv
 ./configure --prefix=`pwd`/install
