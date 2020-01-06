b53f242b570073c1aa5f2d5d2642c58663750e46
Jerry <tw0927041027@gmail.com>
2020-01-06 11:35:17 +0800

    fix cmd error

--- kubevirt-free5gc/install_free5gc.sh
+++ kubevirt-free5gc/install_free5gc.sh
@@ -28,8 +28,8 @@ apt-get install -y \
 apt-get clean 
 
 wget -q https://storage.googleapis.com/golang/getgo/installer_linux
-chmod +x installer_linux
-./installer_linux
+chmod +x /installer_linux
+/installer_linux
 source ~/.bash_profile
 rm -f installer_linux
 go get -u -v "github.com/gorilla/mux"
@@ -37,15 +37,15 @@ go get -u -v "golang.org/x/net/http2"
 go get -u -v "golang.org/x/sys/unix"
 
 git clone https://bitbucket.org/nctu_5g/free5gc-stage-1.git
-cd /OpenStack-Tacker-Exercise/kubevirt-free5gc/free5gc-stage-1
+cd /free5gc-stage-1
 autoreconf -iv
 ./configure --prefix=`pwd`/install
 make -j `nproc`
 make install
 grep -r "mongodb://localhost" | awk -F : '{print $1}' | xargs -i sed -i 's/localhost/mongodb/g' {}
 cd /OpenStack-Tacker-Exercise/kubevirt-free5gc
-cp ./free5gc.conf free5gc-stage-1/install/etc/free5gc/
-cp ./amf.conf free5gc-stage-1/install/etc/free5gc/freeDiameter/
-cp ./hss.conf free5gc-stage-1/install/etc/free5gc/freeDiameter/
-cp ./pcrf.conf free5gc-stage-1/install/etc/free5gc/freeDiameter/
-cp ./smf.conf free5gc-stage-1/install/etc/free5gc/freeDiameter/
+cp ./free5gc.conf /free5gc-stage-1/install/etc/free5gc/
+cp ./amf.conf /free5gc-stage-1/install/etc/free5gc/freeDiameter/
+cp ./hss.conf /free5gc-stage-1/install/etc/free5gc/freeDiameter/
+cp ./pcrf.conf /free5gc-stage-1/install/etc/free5gc/freeDiameter/
+cp ./smf.conf /free5gc-stage-1/install/etc/free5gc/freeDiameter/
